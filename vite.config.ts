460aaf51fcda311ce10112ed3a150df12515ab33
abhinavarnav314-collab <abhinavarnav314@gmail.com>
2026-08-29 20:38:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/Omnichat6/', // <-- ADD THIS EXACT LINE HERE
   plugins: [
     react(),
     VitePWA({
